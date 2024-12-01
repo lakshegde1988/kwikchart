d03a0329f0ee172926f6811553f219a49f2a3d34
lakshegde1988 <lakshhegde1988@gmail.com>
2024-12-01 10:40:51 +0530

    Update IntervalSelector.svelte

--- src/lib/components/IntervalSelector.svelte
+++ src/lib/components/IntervalSelector.svelte
@@ -17,7 +17,6 @@
 </script>
 
 <div class="relative inline-block">
-    <label for="interval-select" class="block text-sm font-medium text-gray-700 mb-1">Select Interval</label>
   <select
     id="interval-select"
     class="block w-auto pl-3 pr-8 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md shadow-sm"
