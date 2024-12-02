53e72225c4385800dbd3ee37ba95568f7fb94f4b
lakshegde1988 <lakshhegde1988@gmail.com>
2024-12-02 11:06:25 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -105,8 +105,6 @@
     window.addEventListener('resize', updateVHUnit);
     window.addEventListener('orientationchange', updateVHUnit);
   
-    loadStocksFromFile(selectedFile);
-
     return () => {
       window.removeEventListener('resize', updateVHUnit);
       window.removeEventListener('orientationchange', updateVHUnit);
@@ -142,6 +140,20 @@
   <footer class="h-16 flex-shrink-0 bg-white border-t border-gray-200 shadow-md">
     <div class="max-w-7xl mx-auto px-4 h-full flex items-center justify-between space-x-4">
       <!-- Left: Selectors -->
+
+ <!-- Fullscreen Button -->
+        <button
+          class="p-2 text-gray rounded-md lg:hidden flex items-center justify-center"
+          on:click={toggleFullscreen}
+        >
+          <div class="w-5 h-5">
+            {#if isFullscreen}
+              <FaCompress />
+            {:else}
+              <FaExpand />
+            {/if}
+          </div>
+        </button>
       <div class="flex items-center space-x-2 sm:space-x-4">
         <IndexSelector class="text-sm sm:text-base px-2" on:select={handleIndexSelect} />
         <IntervalSelector class="text-sm sm:text-base px-2" on:change={handleIntervalChange} />
@@ -169,19 +181,7 @@
           </div>
         </button>
 
-        <!-- Fullscreen Button -->
-        <button
-          class="p-2 bg-blue-500 text-white rounded-md lg:hidden flex items-center justify-center"
-          on:click={toggleFullscreen}
-        >
-          <div class="w-5 h-5">
-            {#if isFullscreen}
-              <FaCompress />
-            {:else}
-              <FaExpand />
-            {/if}
-          </div>
-        </button>
+       
       </div>
     </div>
   </footer>
