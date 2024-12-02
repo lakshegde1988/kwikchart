0cc9fbfdf28c68e45fd2ab398b09756fcd763190
lakshegde1988 <lakshhegde1988@gmail.com>
2024-12-02 10:06:14 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,16 @@ export default {
     "./src/**/*.{svelte,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Customizing theme if needed
+      colors: {
+        lightBackground: "#f8f9fa",
+        darkBackground: "#1a202c",
+        lightText: "#212529",
+        darkText: "#f8f9fa",
+      },
+    },
   },
+  darkMode: "class", // Enable class-based dark mode
   plugins: [],
-}
-
+};
