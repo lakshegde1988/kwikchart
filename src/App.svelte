63b5fd882586f9654c03bd5a59e3eb9cd573128a
lakshegde1988 <lakshhegde1988@gmail.com>
2024-12-04 10:39:59 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
 
   let currentIndex = 0;
   let selectedFile = 'largecap.json';
-  let selectedInterval: Interval = { label: 'Daily', value: '1d', range: '1y' };
+  let selectedInterval: Interval = { label: 'Daily', value: '1d', range: '6mo' };
   let isFullscreen = false;
   let showFavoritesModal = false;
 
