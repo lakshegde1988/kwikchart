4ca0d72629ab2bb60480f1d728505bb15ace671b
lakshegde1988 <lakshhegde1988@gmail.com>
2025-01-03 13:30:10 +0530

    Update StockChart.svelte

--- src/lib/components/StockChart.svelte
+++ src/lib/components/StockChart.svelte
@@ -80,7 +80,7 @@
       timeScale: {
         timeVisible: false,
         rightOffset: 10,
-        minBarSpacing: 8,
+        minBarSpacing: 10,
         borderColor: '#e5e7eb',
       },
     });
