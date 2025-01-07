e2d6381ee270c59edc04006df6f1e8104362d211
lakshegde1988 <lakshhegde1988@gmail.com>
2025-01-07 11:16:44 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -16,7 +16,7 @@
 
   let currentIndex = 0;
   let selectedFile = 'largecaps.json';
-  let selectedInterval: Interval = { label: 'D', value: '1d', range: '2y' };
+  let selectedInterval: Interval = { label: 'D', value: '1d', range: '5y' };
   let isFullscreen = false;
   let showFavoritesModal = false;
   let showTradingViewModal = false;
