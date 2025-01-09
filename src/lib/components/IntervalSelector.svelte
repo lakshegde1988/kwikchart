e522d0eccc47c667574f666207561a8a9b8afbca
lakshegde1988 <lakshhegde1988@gmail.com>
2025-01-09 12:15:03 +0530

    Update IntervalSelector.svelte

--- src/lib/components/IntervalSelector.svelte
+++ src/lib/components/IntervalSelector.svelte
@@ -7,7 +7,7 @@
 
   const intervals: Interval[] = [
     { label: 'Q', value: '1d', range: '3mo' },
-    { label: 'D', value: '1d', range: '5y' },
+    { label: 'D', value: '1d', range: '2y' },
     { label: 'W', value: '1wk', range: '10y' },
     { label: 'M', value: '1mo', range: 'max' }
   ];
