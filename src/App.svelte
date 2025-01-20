ebfec4d886f797c4ad2697921f97d6afed49ccbf
lakshegde1988 <lakshhegde1988@gmail.com>
2025-01-20 21:32:24 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,7 @@
 
   let currentIndex = 0;
   let selectedFile = 'nifty50.json';
-  let selectedInterval: Interval = { label: 'H', value: '1d', range: '6mo' };
+  let selectedInterval: Interval = { label: 'D', value: '1d', range: '6mo' };
   let isFullscreen = false;
   let showFavoritesModal = false;
   let showTradingViewModal = false;
