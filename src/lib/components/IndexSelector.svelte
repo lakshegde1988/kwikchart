330d5456cef6b752c159066556c677f29c6c720f
lakshegde1988 <lakshhegde1988@gmail.com>
2025-01-21 12:00:23 +0530

    Update IndexSelector.svelte

--- src/lib/components/IndexSelector.svelte
+++ src/lib/components/IndexSelector.svelte
@@ -4,7 +4,7 @@
 
   const dispatch = createEventDispatcher<{ select: string }>();
 
-  const indexFiles = ['nifty50.json','next50.json', 'midcaps.json', 'smallcaps.json', 'microcaps.json'];
+  const indexFiles = ['nifty50.json','next50.json', 'midcaps.json', 'smallcaps.json', 'microcaps.json','others.json'];
 
   function handleSelect(event: Event) {
     const selectedFile = (event.target as HTMLSelectElement).value;
