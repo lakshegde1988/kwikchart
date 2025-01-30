ddd3cececb115dad3e6b62e81ece7222b1376d16
lakshegde1988 <lakshhegde1988@gmail.com>
2025-01-30 21:31:14 +0530

    Update IntervalSelector.svelte

--- src/lib/components/IntervalSelector.svelte
+++ src/lib/components/IntervalSelector.svelte
@@ -6,39 +6,38 @@
   const dispatch = createEventDispatcher<{ change: Interval }>();
 
   const intervals: Interval[] = [
-     
-     { label: '6M', value: '1d', range: '6mo' },
-     { label: '1Y', value: '1d', range: '1y' },
-     { label: '2Y', value: '1wk', range: '2y' },
-     { label: '5Y', value: '1wk', range: '5y' },
-     { label: 'XY', value: '1mo', range: '10y' },
-     { label: 'XY', value: '1mo', range: 'max' }
+    { label: '6M', value: '1d', range: '6mo' },
+    { label: '1Y', value: '1d', range: '1y' },
+    { label: '2Y', value: '1wk', range: '2y' },
+    { label: '5Y', value: '1wk', range: '5y' },
+    { label: 'XY', value: '1mo', range: '10y' },
+    { label: 'XY', value: '1mo', range: 'max' }
   ];
 
-  function handleChange(event: Event) {
-    const selectedInterval = intervals[parseInt((event.target as HTMLSelectElement).value)];
-    dispatch('change', selectedInterval);
+  function handleChange(interval: Interval) {
+    dispatch('change', interval);
   }
 </script>
 
 <div class="relative inline-block">
-  <select
-    id="interval-select"
-    class="block w-auto text-base focus:outline-none focus:ring-2 focus:ring-opacity-50 sm:text-sm rounded-md shadow-sm"
-    class:bg-slate-50={$theme === 'light'}
-    class:text-slate-900={$theme === 'light'}
-    class:border-slate-300={$theme === 'light'}
-    class:focus:ring-slate-500={$theme === 'light'}
-    class:focus:border-slate-50={$theme === 'light'}
-    class:bg-slate-950={$theme === 'dark'}
-    class:text-slate-100={$theme === 'dark'}
-    class:border-slate-950={$theme === 'dark'}
-    class:focus:ring-slate-950={$theme === 'dark'}
-    class:focus:border-slate-950={$theme === 'dark'}
-    on:change={handleChange}
-  >
-    {#each intervals as interval, i}
-      <option value={i}>{interval.label}</option>
+  <div class="flex space-x-2">
+    {#each intervals as interval}
+      <button
+        class="px-4 py-2 text-base focus:outline-none focus:ring-2 focus:ring-opacity-50 sm:text-sm rounded-md shadow-sm"
+        class:bg-slate-50={$theme === 'light'}
+        class:text-slate-900={$theme === 'light'}
+        class:border-slate-300={$theme === 'light'}
+        class:focus:ring-slate-500={$theme === 'light'}
+        class:focus:border-slate-50={$theme === 'light'}
+        class:bg-slate-950={$theme === 'dark'}
+        class:text-slate-100={$theme === 'dark'}
+        class:border-slate-950={$theme === 'dark'}
+        class:focus:ring-slate-950={$theme === 'dark'}
+        class:focus:border-slate-950={$theme === 'dark'}
+        on:click={() => handleChange(interval)}
+      >
+        {interval.label}
+      </button>
     {/each}
-  </select>
+  </div>
 </div>
