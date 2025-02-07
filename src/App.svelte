b2ff5cb510976bfe15a80c1235641538919686ba
lakshegde1988 <lakshhegde1988@gmail.com>
2025-02-07 12:48:57 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -172,7 +172,7 @@
   class:text-slate-900={$theme === 'light'}
   class:bg-black={$theme === 'dark'}
   class:text-slate-50={$theme === 'dark'}
-  style="height: 350px"
+  style="height: 420px"
 >
   <!-- Content Area -->
   <div class=" flex flex-grow overflow-auto">
