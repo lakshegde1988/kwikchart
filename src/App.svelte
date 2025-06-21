7a9ae83b420a76a0dec4e496da59ef9ade1e00ae
lakshegde1988 <lakshhegde1988@gmail.com>
2025-06-21 09:01:52 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -224,7 +224,7 @@
         disabled={currentIndex === 0}
       >
         <ArrowLeft class="w-5 h-5" />
-        <span class="hidden sm:inline">Prev</span>
+        <span class="sm:inline">Prev</span>
       </button>
       <button
         class=" items-center gap-2 py-2 px-2"
@@ -233,7 +233,7 @@
         on:click={handleNext}
         disabled={currentIndex === totalStocks - 1}
       >
-        <span class="hidden sm:inline">Next</span>
+        <span class="sm:inline">Next</span>
         <ArrowRight class="w-5 h-5" />
       </button>
     </div>
